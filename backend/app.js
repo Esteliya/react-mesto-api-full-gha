6e3ab7f83d9dfc9b68f7c6fec109c67e7fbc8bf0
Esteliya <avroradis@gmail.com>
2023-07-28 05:50:26 +0300

    убираем путь /api/

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ const {
   DB_PRODUCTION,
   PORT = 3000,
   DB_URL = 'mongodb://localhost:27017/mestodb_new',
+  // DB_URL = 'mongodb://127.0.0.1:27017/mestodb_new',
   // бэк???
   // DB_URL = 'api.avroradis.students.nomoreparties.sbs',
 } = process.env;
